5b3cde4980481bdf7c9510fc173d900169a784bc
Praveenezhil20 <ezhilarasusivam@gmail.com>
2024-09-02 20:25:59 +0530

    final commit

--- src/App.js
+++ src/App.js
@@ -3,15 +3,37 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Welcomepage from './components/Welcomepage';
 import './App.css'
-import AnotherPage from './components/AnotherPage'; // Import your target page component
+// import AnotherPage from './components/AnotherPage'; // Import your target page component
 import Signup from './components/signup'; 
+import Dashboard from './Pages/Dashboard';
+import Learning from './Pages/Learning';
+import Projects from './Pages/Projects';
+import Tasks from './Pages/Tasks';
+import Attendance from './Pages/Attendance';
+import People from './Pages/People';
+import ManageLeave from './Pages/ManageLeave';
+import Settings from './Pages/Settings';
+import Sidebar from './SideBar/Sidebar';
+import Notification from './Pages/Notification';
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Welcomepage />} />
-        <Route path="/another" element={<AnotherPage />} />
-        <Route path="/signup" element={< Signup/>} /> {/* Define your route */}
+        <Route path="/signup" element={< Signup/>} /> 
+        <Route element={<Sidebar/>}>
+        <Route path="/deshboard" element={<Dashboard/>}/>
+          
+          <Route path="/learning" element={<Learning />} />
+          <Route path="/projects" element={<Projects />} />
+          <Route path="/tasks" element={<Tasks/>} />
+          <Route path="/attendance" element={<Attendance />} />
+          <Route path="/mange leave" element={<ManageLeave />} />
+          <Route path="/people" element={<People />} />
+          <Route path="/settings" element={<Settings />} />
+          <Route path="/notification" element={<Notification />} />
+          <Route path="/rectangle" element={<Settings />} />
+          </Route>{/* Define your route */}
       </Routes>
     </Router>
     
--- src/components/Welcomepage.jsx
+++ src/components/Welcomepage.jsx
@@ -1,26 +1,27 @@
 import React, { useState } from 'react';
 import { Container, Grid, TextField, Button, Typography, Link, Box, InputAdornment, IconButton } from '@mui/material';
 import { useNavigate } from 'react-router-dom';
-import Welcomeimage from '../components/Welcomeimage.png';
-import Logo from '../components/Logo.png'; 
-import NewLogo from '../components/NewLogo.png'; 
-import Visibility from '@mui/icons-material/Visibility'; 
-import VisibilityOff from '@mui/icons-material/VisibilityOff'; 
+import Welcomeimage from './Welcomeimage.png';
+import Logo from './Logo.png';
+import NewLogo from './NewLogo.png';
+import Visibility from '@mui/icons-material/Visibility';
+import VisibilityOff from '@mui/icons-material/VisibilityOff';
+import {LoginSocialGoogle} from 'reactjs-social-login'
 
 function Welcomepage() {
-  const [email, setEmail] = useState(''); 
-  const [password, setPassword] = useState(''); 
-  const [showPassword, setShowPassword] = useState(false); 
-  const [emailError, setEmailError] = useState(''); 
-  const [passwordError, setPasswordError] = useState(''); 
-  const navigate = useNavigate(); 
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [showPassword, setShowPassword] = useState(false);
+  const [emailError, setEmailError] = useState('');
+  const [passwordError, setPasswordError] = useState('');
+  const navigate = useNavigate();
 
   const handleClickShowPassword = () => {
-    setShowPassword(!showPassword); 
+    setShowPassword(!showPassword);
   };
 
   const handleMouseDownPassword = (event) => {
-    event.preventDefault(); 
+    event.preventDefault();
   };
 
   const validateForm = () => {
@@ -45,32 +46,41 @@ function Welcomepage() {
     return isValid;
   };
 
-  const handleLoginClick = () => {
+  const handleLoginClick = (event) => {
+    event.preventDefault();
     if (validateForm()) {
-      navigate('/another'); 
+      navigate('/deshboard');
     }
   };
 
   const handleSignUpClick = () => {
-    navigate('/signup'); // Navigate to sign-up page
+    navigate('/signup');
   };
 
   return (
-    <Container maxWidth="md" sx={{ marginTop: { xs: '20px', sm: '30px', md: '50px' } }}>
+
+    <Container height='100%' maxWidth="md" sx={{ marginTop: {md: "90px"}, display: 'flex',flexDirection:"column", alignItems: "center",justifyContent:"center" }}>
       <Box
         width="100%"
-        p={{ xs: 2, sm: 3, md: 4 }} // Responsive padding
-        borderRadius={2} // Rounded corners
-        boxShadow={{ xs: 1, sm: 2, md: 3 }} // Responsive box shadow
-        bgcolor="#fff" // Background color
+        p={{ xs: 2, sm: 3, md: 4 }}
+        borderRadius={2}
+        boxShadow={{ xs: 1, sm: 2, md: 3 }}
+        bgcolor="#fff"
         display="flex"
         flexDirection="column"
         alignItems="center"
+        justifyContent='center'
+        height='100%'
       >
         <Grid container spacing={{ xs: 2, sm: 3, md: 4 }} alignItems="center" justifyContent="center">
-          
-          {/* Content section */}
-          <Grid item xs={12} md={6} container alignItems="center" justifyContent="center">
+          <Grid
+            item
+            xs={12}
+            md={6}
+            container
+            alignItems="center"
+            justifyContent="center"
+          >
             <Box width="100%" textAlign="center">
               <div>
                 <img
@@ -80,31 +90,44 @@ function Welcomepage() {
                 />
                 <br />
               </div>
-              <Typography variant="h4" gutterBottom>
+              <Typography variant="h4" gutterBottom sx={{ fontWeight: 'bold' }}>
                 Welcome Back!
-                <Typography variant="subtitle1" sx={{ color: '#5B738B' }} gutterBottom>
-                  Login to your account
-                </Typography>
               </Typography>
+              <Typography
+                variant="subtitle1"
+                sx={{ color: '#5B738B' }}
+                gutterBottom
+              >
+                Login to your account
+              </Typography>
+
               <Box textAlign="left" mt={2}>
                 <Typography variant="body1" color="#5B738B">Email ID</Typography>
                 <TextField
                   variant="outlined"
                   placeholder="Enter your Email ID"
                   fullWidth
-                  margin="normal"
+                  margin="none" // Changed from "0" to "none"
+                  sx={{
+                    backgroundColor: '#F1ECFF',
+                  }}
                   value={email}
                   onChange={(e) => setEmail(e.target.value)}
                   error={Boolean(emailError)}
                   helperText={emailError}
                 />
-                <Typography variant="body1" color="#5B738B" mt={2}>Password</Typography>
+                <br />
+                <br />
+                <Typography variant="body1" color="#5B738B">Password</Typography>
                 <TextField
                   variant="outlined"
                   placeholder="Enter your Password"
                   type={showPassword ? 'text' : 'password'}
                   fullWidth
-                  margin="normal"
+                  margin="none"
+                  sx={{
+                    backgroundColor: '#F1ECFF',
+                  }}
                   value={password}
                   onChange={(e) => setPassword(e.target.value)}
                   error={Boolean(passwordError)}
@@ -121,24 +144,24 @@ function Welcomepage() {
                           {showPassword ? <Visibility /> : <VisibilityOff />}
                         </IconButton>
                       </InputAdornment>
-                    ),
+                    )
                   }}
                 />
+
               </Box>
-              <Box display="flex" justifyContent="center" mb={2} mt={2}>
-                <Link href="#" variant="body2" color="#5B738B" sx={{ marginRight: '10px' }}>
+              <Box display="flex" justifyContent="center" mb={2} mt={2} >
+                <Link href="#" variant="body2" color="#5B738B" style={{ marginRight: '10px', textDecoration: 'none' }}>
                   Forgot Password?
                 </Link>
-                <Link href="#" variant="body2" color="#A668F4" fontWeight={'400'}>
+                <Link href="#" variant="body2" color="#A668F4" fontWeight={'400'} style={{ textDecoration: 'none' }}>
                   Reset Password
                 </Link>
               </Box>
-              
               <Button
                 variant="contained"
-                sx={{ 
-                  marginTop: '20px', 
-                  fontSize: '16px', 
+                style={{
+                  marginTop: '20px',
+                  fontSize: '16px',
                   textTransform: 'capitalize',
                   backgroundColor: '#A668F4',
                   color: '#ffffff'
@@ -149,57 +172,82 @@ function Welcomepage() {
               >
                 Login
               </Button>
-
-              <Box 
-                display="flex" 
-                alignItems="center" 
-                justifyContent="center" 
-                sx={{ margin: '20px 0' }}
+              <Box
+                display="flex"
+                alignItems="center"
+                justifyContent="center"
+                style={{ margin: '20px 0' }}
               >
                 <hr style={{ flex: 1, border: '0', borderTop: '1px solid #000', marginRight: '8px' }} />
-                <Typography variant="body2" sx={{ whiteSpace: 'nowrap' }}>
+                <Typography variant="body2" style={{ whiteSpace: 'nowrap' }}>
                   or
                 </Typography>
                 <hr style={{ flex: 1, border: '0', borderTop: '1px solid #000', marginLeft: '8px' }} />
               </Box>
+              <LoginSocialGoogle
+                  client_id='208154501561-6202i49b0pmhn22c6fkvqqr7eepdson0.apps.googleusercontent.com'
+                  access_type='offline'
+                  
+                  onResolve={(provider,data) => {
+
+
+                  }}
+                  >
+                    <Button
+                      variant="outlined"
+                      startIcon={<img src={NewLogo} alt="New Logo" style={{width:20,height:20}} />}
+                      fullWidth
+                      style={{
+                        marginBottom:'20px',
+                        textTransform:'none',
+                        borderColor:'#000000',
+                        color:'#000000',
+                        backgroundColor:'transparent',
+                        fontWeight:'800'
+                      }}
+                    >
+                    Login with Google
+                    </Button>
+                  </LoginSocialGoogle>
               
-              <Button
-                variant="outlined"
-                startIcon={<img src={NewLogo} alt="New Logo" style={{ width: 20, height: 20 }} />}
-                fullWidth
-                sx={{ 
-                  marginBottom: '20px',
-                  textTransform: 'capitalize',
-                  borderColor: '#000000',
-                  color: '#000000',
-                  backgroundColor: 'transparent'
-                }}
-              >
-                Login with Google
-              </Button>
+
+              
+                
+                
+              
 
               <Typography variant="body2" align="center" sx={{ color: '#5B738B' }}>
                 Don’t have an account? {' '}
-                <Link href="#" color="#A668F4" onClick={handleSignUpClick}>
+                <Link href="#" color="#A668F4" onClick={handleSignUpClick} style={{ textDecoration: 'none' }}>
                   Sign up for Free
                 </Link>
               </Typography>
             </Box>
           </Grid>
-          
-          {/* Welcome image section */}
           <Grid item xs={12} md={6} container alignItems="center" justifyContent="center">
             <Box
               width="100%"
               display="flex"
               justifyContent="center"
               alignItems="center"
-              sx={{ display: { xs: 'none', md: 'block' } }}
+              sx={{
+                backgroundColor: '#F1ECFF',
+                border: '1px solid #ccc',
+                height: '600px',
+                borderRadius: '8px',
+                padding: '8px',
+                display: { xs: 'none', md: 'block' },
+              }}
             >
               <img
                 src={Welcomeimage}
                 alt="Illustration"
-                style={{ maxWidth: '90%', maxHeight: '80vh', objectFit: 'contain', borderRadius: '8px', marginLeft: '20px' }}
+                style={{
+                  maxWidth: '100%',
+                  height: '100%',
+                  objectFit: 'contain',
+                  borderRadius: '8px',
+                }}
               />
             </Box>
           </Grid>
